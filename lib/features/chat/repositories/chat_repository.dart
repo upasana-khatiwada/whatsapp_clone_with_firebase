3318c67ddf36d0d9fb77c954bac1d7da23fcc8b3
upasana khatiwada <upashanakhatiwada1234@gmail.com>
2024-01-18 10:41:26 +0545

    save data to contacts of collection

--- lib/features/chat/repositories/chat_repository.dart
+++ lib/features/chat/repositories/chat_repository.dart
@@ -1,9 +1,86 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/models/chat_contact.dart';
+
+import '../../../common/utils/utils.dart';
+import '../../../models/user_model.dart';
 
 class ChatRepository {
   final FirebaseFirestore firestore;
   final FirebaseAuth auth;
 
-  ChatRepository({required this.firestore, required this.auth});
+  ChatRepository({
+    required this.firestore,
+    required this.auth,
+  });
+
+  void _saveDataToContactsSubcollection(
+    UserModel senderUserData,
+    UserModel recieverUserData,
+    String text,
+    DateTime timeSent,
+    String recieverUserId,
+//bool isGroupChat,
+  ) async {
+    //users => receiver user Id => chats -> current user ID -> set data
+    var recieverChatContact = ChatContact(
+      name: senderUserData.name,
+      profilePic: senderUserData.profilePic,
+      contactId: senderUserData.uid,
+      timeSent: timeSent,
+      lastMessage: text,
+    );
+    await firestore
+        .collection('users')
+        .doc(recieverUserId)
+        .collection('chats')
+        .doc(auth.currentUser!.uid)
+        .set(recieverChatContact.toMap());
+    //users => current user Id => chats -> receiver user ID -> set data
+    var senderChatContact = ChatContact(
+      name: recieverUserData.name,
+      profilePic: recieverUserData.profilePic,
+      contactId: recieverUserData.uid,
+      timeSent: timeSent,
+      lastMessage: text,
+    );
+    await firestore
+        .collection('users')
+        .doc(auth.currentUser!.uid)
+        .collection('chats')
+        .doc(recieverUserId)
+        .set(senderChatContact.toMap());
+  }
+
+//process
+//usercollection ->senderid->receiver id -> messages ->message id ->store message
+
+  void sendTextMessage({
+    required BuildContext context,
+    required String text,
+    required String recieverUserId,
+    required UserModel senderUser,
+    // required MessageReply? messageReply,
+    required bool isGroupChat,
+  }) async {
+    try {
+      var timeSent = DateTime.now();
+      UserModel? recieverUserData;
+
+      var userDataMap =
+          await firestore.collection('users').doc(recieverUserId).get();
+      recieverUserData = UserModel.fromMap(userDataMap.data()!);
+
+      _saveDataToContactsSubcollection(
+        senderUser,
+        recieverUserData,
+        text,
+        timeSent,
+        recieverUserId,
+      ) {}
+    } catch (e) {
+      showSnackBar(context: context, content: e.toString());
+    }
+  }
 }
