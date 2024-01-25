a712dbc05255285961dfe866b1b046920885cf92
upasana khatiwada <upashanakhatiwada1234@gmail.com>
2024-01-25 16:54:49 +0545

    Recording & Sharing Audio in Flutter App

--- lib/features/chat/widgets/display_text_image_gif.dart
+++ lib/features/chat/widgets/display_text_image_gif.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/common/enums/message_enum.dart';
@@ -14,6 +15,8 @@ class DisplayTextImageGIF extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isPlaying = false;
+    final AudioPlayer audioPlayer = AudioPlayer();
     return type == MessageEnum.text
         ? Text(
             message,
@@ -22,15 +25,29 @@ class DisplayTextImageGIF extends StatelessWidget {
             ),
           )
         : type == MessageEnum.audio
-            ? IconButton(
-                constraints: const BoxConstraints(
-                  minWidth: 100,
-                ),
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.play_circle,
-                ),
-              )
+            ? StatefulBuilder(builder: (context, setState) {
+                return IconButton(
+                  constraints: const BoxConstraints(
+                    minWidth: 100,
+                  ),
+                  onPressed: () async {
+                    if (isPlaying) {
+                      await audioPlayer.pause();
+                      setState(() {
+                        isPlaying = false;
+                      });
+                    } else {
+                      await audioPlayer.play(UrlSource(message));
+                      setState(() {
+                        isPlaying = true;
+                      });
+                    }
+                  },
+                  icon: Icon(
+                    isPlaying ? Icons.pause_circle : Icons.play_circle,
+                  ),
+                );
+              })
             : type == MessageEnum.video
                 ? VideoPlayerItem(
                     videoUrl: message,
